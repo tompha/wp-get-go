7ecac187335d4f0cb54e0c7145aa83f796b7361d
Matt Royce <matt@abitgooey.com>
2017-07-27 12:23:20 +0100

    Start re-build of theme

--- gulpfile.js
+++ gulpfile.js
@@ -1,95 +1,149 @@
 "use strict";
 
-/**
- * Required packages
- */
-const gulp        = require('gulp');
-const sass        = require('gulp-sass');
-const sourcemap   = require('gulp-sourcemaps');
-const prefix      = require('gulp-autoprefixer');
-const concat 	  = require('gulp-concat');
-const watch       = require('gulp-watch');
-const notifier    = require('node-notifier');
-const colors      = require('colors');
-const util        = require('gulp-util');
-const uglify      = require('gulp-uglify');
-const argv        = require('yargs').argv;
-const babel       = require('gulp-babel');
-const browserify  = require('browserify');
-const babelify    = require('babelify');
-const source      = require('vinyl-source-stream');
-const buffer      = require('vinyl-buffer');
+const Packages = {
+    General: {
+        gulp:           require('gulp-help')(require('gulp')),
+        watch:          require('gulp-watch'),
+        sourcemaps:     require('gulp-sourcemaps'),
+        plumber:        require('gulp-plumber')
+    },
+    Styles: {
+        sass:           require('gulp-sass'),
+        autoprefixer:   require('gulp-autoprefixer'),
+        concat:         require('gulp-concat')
+    },
+    Scripts: {
+        babel:          require('gulp-babel'),
+        browserify:     require('browserify'),
+        babelify:       require('babelify'),
+        uglify:         require('gulp-uglify')
+    },
+    Tools: {
+        notifier:       require('node-notifier'),
+        colors:         require('colors'),
+        util:           require('gulp-util'),
+        argv:           require('yargs').argv,
+        source:         require('vinyl-source-stream'),
+        buffer:         require('vinyl-buffer')
+    }
+};
+
+const Options = {
+    styles: {
+        path: 'app/src/styles/main.scss',
+        name: {
+            development: 'theme.css',
+            production:  'theme.min.css'
+        },
+        output: 'app/dist/styles/',
+        options: {
+            outputStyle: Packages.Tools.argv.compress ? 'compressed' : 'expanded'
+        }
+    },
+    scripts: {
+        output: 'app/dist/scripts/',
+        targets: [
+            {
+                path:       'app/src/scripts/libraries/**/*.js',
+                browserify: false,
+                transpile:  false,
+                name:       {
+                    development: 'libraries.js',
+                    production:  'libraries.min.js'
+                }
+            },
+            {
+                path:       'app/src/scripts/plugins/**/*.js',
+                browserify: false,
+                transpile:  true,
+                name:       {
+                    development: 'plugins.js',
+                    production:  'plugins.min.js'
+                }
+            },
+            {
+                path:       'app/src/scripts/theme/app.js',
+                browserify: true,
+                transpile:  false,
+                name:       {
+                    development: 'app.js',
+                    production:  'app.min.js'
+                }
+            }
+        ]
+    },
+    plumber: {
+        errorHandler: compilerError
+    }
+};
 
 /**
  * Task definitions
  */
-gulp.task('compile:styles', function() {
-    gulp.src('./src/scss/main.scss')
-    .pipe(sass({ outputStyle: (argv.compress ? 'compressed' : 'expanded' ) })).on('error', compilerError)
-    .pipe(prefix('last 2 versions')).on('error', compilerError)
-    .pipe(concat('theme.css')).on('error', compilerError)
-    .pipe(argv.compress ? util.noop() : sourcemap.write('maps')).on('error', compilerError)
-    .pipe(gulp.dest('./assets/css/'));
+Packages.General.gulp.task('compile:styles', function() {
+    let compress = !!Packages.Tools.argv.compress;
+
+    Packages.General.gulp.src(Options.styles.path)
+        .pipe(Packages.General.plumber(Options.plumber))
+        .pipe(Packages.Styles.sass(Options.styles.options))
+        .pipe(Packages.Styles.autoprefixer('last 2 versions'))
+        .pipe(Packages.Styles.concat(compress ? Options.styles.name.production : Options.styles.name.development))
+        // .pipe(compress ? util.noop() : sourcemap.write('maps'))
+        .pipe(Packages.General.gulp.dest(Options.styles.output));
 });
 
-gulp.task('compile:scripts', function() {
-    let _targets = {
-        libraries: {
-            targetName: 'libraries.js',
-            targetPath: './src/js/libraries/**/*.js',
-            browserify: false,
-            transpile:  false
-        },
-        plugins: {
-            targetName: 'plugins.js',
-            targetPath: './src/js/plugins/**/*.js',
-            browserify: false,
-            transpile:  true
-        },
-        theme: {
-            targetName: 'theme.js',
-            targetPath: './src/js/theme/main.js',
-            browserify: true
-        }
-    };
+Packages.General.gulp.task('compile:scripts', function() {
+    let compress = !!Packages.Tools.argv.compress;
 
-    for (let target in _targets) {
-        let bundle;
+    for (let i = 0, total = Options.scripts.targets.length; i < total; i++) {
+        let stream;
+        let target = Options.scripts.targets[i];
+        let name = compress ? target.name.production : target.name.development;
 
-        if (_targets[target].browserify) {
-            bundle = browserify(_targets[target].targetPath).transform(babelify, {
-                presets: ['es2015'],
-                compact: false
+        if (target.browserify) {
+            stream = Packages.Scripts.browserify({
+                entries: target.path,
+                debug: compress,
+                transform: [Packages.Scripts.babelify.configure({
+                    presets: ['es2015']
+                })]
             }).bundle();
 
-            bundle = bundle.pipe(source(_targets[target].targetName));
-            bundle = bundle.pipe(buffer());
+            stream = stream.pipe(Packages.Tools.source(name));
+            stream = stream.pipe(Packages.Tools.buffer());
         } else {
-            bundle = gulp.src(_targets[target].targetPath);
+            stream = Packages.General.gulp.src(target.path);
+            stream = stream.pipe(Packages.General.plumber(Options.plumber));
 
-            if (_targets[target].transpile) {
-                bundle = bundle.pipe(babel({presets: ['es2015'], compact: false})).on('error', compilerError);
+            if (target.transpile) {
+                stream = stream.pipe(Packages.Scripts.babel({
+                    presets: ['es2015'],
+                    compact: compress
+                }));
             }
 
-            bundle = bundle.pipe(concat(_targets[target].targetName)).on('error', compilerError);
+            stream = stream.pipe(Packages.Styles.concat(name));
         }
 
-        bundle = bundle.pipe(argv.compress ? uglify() : util.noop()).on('error', compilerError);
-        bundle.pipe(gulp.dest('./assets/js/'));
+        if (compress) {
+            stream = stream.pipe(Packages.Scripts.uglify());
+        }
+
+        stream.pipe(Packages.General.gulp.dest(Options.scripts.output));
     }
 });
 
-gulp.task('watch:styles', function() {
-    gulp.watch('./src/scss/**/*.scss', ['compile:styles']);
+Packages.General.gulp.task('watch:styles', function() {
+    Packages.General.watch('./src/scss/**/*.scss', ['compile:styles']);
 });
 
-gulp.task('watch:scripts', function() {
+Packages.General.gulp.task('watch:scripts', function() {
     gulp.watch('./src/js/**/*.js', ['compile:scripts']);
 });
 
-gulp.task('watch', ['watch:styles', 'watch:scripts']);
-gulp.task('compile', ['compile:styles', 'compile:scripts']);
-gulp.task('default', ['compile']);
+Packages.General.gulp.task('watch', ['watch:styles', 'watch:scripts']);
+Packages.General.gulp.task('compile', ['compile:styles', 'compile:scripts']);
+Packages.General.gulp.task('default', ['compile']);
 
 /**
  * Error handling
@@ -100,7 +154,7 @@ function compilerError(error) {
         title: 'Compilation error'
     };
 
-    util.log(colors.red('COMPILATION ERROR'));
+    Packages.Tools.util.log(colors.red('COMPILATION ERROR'));
 
     if (Object.keys(errorObject).length > 0) {
         console.log('File: ' + errorObject.relativePath);
@@ -111,6 +165,6 @@ function compilerError(error) {
         console.log(error);
     }
 
-    notifier.notify(notificationArgs);
+    Packages.Tools.notifier.notify(notificationArgs);
     this.emit('end');
 }
