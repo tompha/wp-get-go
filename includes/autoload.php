4447d80d89b242e6e8815a7e1b74774c412ebe56
Matt Royce <hello@mattroyce.org>
2017-11-30 17:33:05 +0000

    Re-factor some files back to 4-spaces

--- includes/autoload.php
+++ includes/autoload.php
@@ -8,15 +8,15 @@
 $directories = ['classes', 'theme', 'ajax'];
 
 foreach ($directories as $directory) {
-	$path = __DIR__ . '/' . $directory;
+    $path = __DIR__ . '/' . $directory;
 
-	if (file_exists($path)) {
-		$files = glob($path . '/*.php');
+    if (file_exists($path)) {
+        $files = glob($path . '/*.php');
 
-		if (!empty($files)) {
-			foreach ($files as $file) {
-				require_once $file;
-			}
-		}
-	}
+        if (!empty($files)) {
+            foreach ($files as $file) {
+                require_once $file;
+            }
+        }
+    }
 }
--- includes/theme/hooks.php
+++ includes/theme/hooks.php
@@ -12,34 +12,34 @@ add_action('init', 'register_custom_taxonomies');
 
 // load any assets needed
 add_action('init', function() {
-	if (is_admin() || in_array($GLOBALS['pagenow'], array('wp-register.php', 'wp-login.php'))) {
-		return;
-	}
+    if (is_admin() || in_array($GLOBALS['pagenow'], ['wp-register.php', 'wp-login.php'])) {
+        return;
+    }
 
-	Theme::enqueueStyle('app', 'app');
-	Theme::enqueueScript('app', 'app');
+    Theme::enqueueStyle('app', 'app');
+    Theme::enqueueScript('app', 'app');
 
-	// remove WordPress' version of jQuery on the front-end
-	if (wp_script_is('jquery', 'registered')) {
-		wp_deregister_script('jquery');
-	}
+    // remove WordPress' version of jQuery on the front-end
+    if (wp_script_is('jquery', 'registered')) {
+        wp_deregister_script('jquery');
+    }
 });
 
 // register any additional functionality needed
 add_action('after_setup_theme', function() {
-	// theme support
-	add_theme_support('html5');
-	add_theme_support('post-thumbnails');
+    // theme support
+    add_theme_support('html5');
+    add_theme_support('post-thumbnails');
 
-	// menus
-	register_nav_menu('main', 'Main Menu');
+    // menus
+    register_nav_menu('main', 'Main Menu');
 
-	// theme options page
-	if (function_exists('acf_add_options_page')) {
-		acf_add_options_page(array(
-			'page_title' => 'Options',
-			'parent_slug' => 'themes.php',
-			'menu_slug' => 'theme-options'
-		));
-	}
+    // theme options page
+    if (function_exists('acf_add_options_page')) {
+        acf_add_options_page([
+            'page_title'  => 'Options',
+            'parent_slug' => 'themes.php',
+            'menu_slug'   => 'theme-options'
+        ]);
+    }
 });
