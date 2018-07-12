67cfd797fe6528a1ca7040946af41bac0f9cd225
Matt Royce <hello@mattroyce.org>
2018-07-12 14:52:05 +0100

    Tidy up the Mix config file and add image minification

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,37 @@
-let mix = require('laravel-mix');
+/**
+ * Laravel Mix
+ * ----------------------------------------------------------------------------
+ * Utilise Mix's webpack wrapper to handle asset compilation and other build
+ * tasks we might need for our builds
+ */
+const { mix } = require('laravel-mix');
+const ImageMinPlugin = require('imagemin-webpack-plugin').default;
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const ImageMinMozPlugin = requre('imagemin-mozjpeg');
 
-mix.js('resources/assets/js/app.js', 'public/js');
-mix.sass('resources/assets/scss/app.scss', 'public/css/');
-mix.browserSync('get-go.wp');
+mix.webpackConfig({
+    devtool: 'source-map',
+    plugins: [
+        new CopyWebpackPlugin([{
+            from: 'resources/assets/image',
+            to: 'public/images'
+        }]),
+        new ImageMinPlugin({
+            test: /\.(jpe?g|png|gif|svg)$/i,
+            plugins: [
+                ImageMinMozPlugin({
+                    quality: 80
+                })
+            ]
+        })
+    ]
+}).options({
+    processCssUrls: false
+});
+
+mix.js('resources/assets/js/app.js', 'public/js').sourceMaps();
+mix.sass('resources/assets/scss/app.scss', 'public/css').sourceMaps();
+
+if (!mix.inProduction()) {
+    mix.browserSync('get-go.wp');
+}
